6f8b29df97df2566c3a9f6621403c19afd893a04
Jeppe Andreasen <jepaan@users.noreply.github.com>
2020-04-28 21:38:37 +0200

    Added Project64-video

--- Source/Project64Qt/main.cpp
+++ Source/Project64Qt/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
+//Test for fun
 #include "Common/Random.h"
 
-int main()
+//From Windows main
+#include "Project64-core/AppInit.h"
+//#include "Multilanguage\LanguageSelector.h"
+//#include "Settings/UISettings.h"
+#include "Common/path.h"
+
+int main(int argc, const char** argv)
 {
     CRandom random;
-  std::cout << "Hello World! " << random.next() << std::endl;
+    std::cout << "Hello World! " << random.next() << std::endl;
+    //CNotification notify;
+    //AppInit(&notify, CPath(CPath::DIR_CURRENT_DIRECTORY), argc, argv);
 }
